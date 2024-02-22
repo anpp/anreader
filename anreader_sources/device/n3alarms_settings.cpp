165e7d95d9b073239808cdf82b21e27851cd3110
anpp <andrey.nik.ponomarenko@gmail.com>
2024-02-22 20:00:40 +0300

    tiny refactoring

--- anreader_sources/device/n3alarms_settings.cpp
+++ anreader_sources/device/n3alarms_settings.cpp
@@ -78,6 +78,7 @@ uint16_t N3AlarmsSettings::altitude(int index, int altindex) const
     if(index >= 0 && index < 8 && m_data.size() > static_cast<int>(as_offsets::beginArray) + (index * 10) + static_cast<int>(as_offsets::altitudeOffset) + (altindex * 2))
     {
         int result =  BytesOperations::getValue16(m_data, static_cast<int>(as_offsets::beginArray) + (index * 10) + static_cast<int>(as_offsets::altitudeOffset) + (altindex * 2));
+        //return result;
         altitude_measure am = (nullptr != m_device_settings) ? m_device_settings->altitudeMeasure() : altitude_measure::feet;
         double stp = step(index);        
         int factor = type(index) == alarm_type::FreeFall ? 100 : 10;
@@ -186,7 +187,7 @@ void N3AlarmsSettings::setAltitude(int index, int altindex, uint16_t value)
         int factor = (type(index) == alarm_type::FreeFall ? 50 : 10);
 
         if(altitude_measure::meters == am)
-            device_altitude = metersIncs2feet(device_altitude * 2.0, factor);
+            device_altitude = metersIncs2feet(device_altitude * 2, factor);
 
         device_altitude = feet2metersIncs(device_altitude, type(index));
         device_altitude = correct_altitude(index, altindex, device_altitude, device_save_altitude);
@@ -285,7 +286,7 @@ unsigned int N3AlarmsSettings::min(int index) const
 //----------------------------------------------------------------------------------------------------------------------
 double N3AlarmsSettings::metersIncs2feet(double value, int step) const
 {
-    double result = round((double) (((((((double)value) / 2.0) * 1000.0) / 25.4) / 12.0) / ((double)step)));
+    double result = round((double) (((((((double)value) / 2.0) * 1000.0) / 25.4) / 12.0) / ((double)step)) - 0.05);
     return result * step;
 }
 
@@ -294,8 +295,7 @@ double N3AlarmsSettings::metersIncs2feet(double value, int step) const
 int N3AlarmsSettings::feet2metersIncs(double value, alarm_type atype) const
 {
     int factor = (atype == alarm_type::FreeFall ? 25 : 5);
-    double result = value * 2;
-    result = round((double) (result * 30.48) * factor);
+    int result = round((double) (value * 2.0 * 30.48) * factor);
     return round((double) (((double) result) / ((double) (100 * factor))));
 }
 
